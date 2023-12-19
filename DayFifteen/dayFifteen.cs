6404924e21d1769d6e57d3f3fe52ad87b296fa84
James McNeil <jamesmcneil42@hotmail.co.uk>
2023-12-19 17:19:46 +0100

    Update dayFifteen.cs to solve part 2

--- DayFifteen/dayFifteen.cs
+++ DayFifteen/dayFifteen.cs
@@ -1,21 +1,62 @@
 var input = File.ReadAllText("input.txt").Replace("\n", "").Replace("\r", "").Split(',');
-PartOne();
+long overAllSum = 0;
+Dictionary<int, List<string>> lenses = new Dictionary<int, List<string>>(256);
 
-void PartOne()
+foreach (var set in input)
 {
-    long overAllSum = 0;
+  int sum = 0;
+  bool lenseSumFinalised = false;
+  int lenseSum = 0;
+  string label = "";
 
-    foreach (var set in input)
+
+  foreach (char c in set)
+  {
+    if (c == '-' || c == '=')
     {
-        long sum = 0;
-
-        foreach(char c in set)
-        {
-            sum = ((sum + c) * 17) % 256;
-        }
-
-        overAllSum += sum;
+      lenseSum = sum;
+      lenseSumFinalised = true;
     }
 
-    Console.WriteLine($"Part One: {overAllSum}");
+    if (!lenseSumFinalised) label += c;
+
+
+    sum = ((sum + c) * 17) % 256;
+  }
+
+  overAllSum += sum;
+  
+  List<string> lenseList = lenses.ContainsKey(lenseSum) ? lenses[lenseSum] : new List<string>();
+  if (set.Contains('='))
+  {
+    int index = lenseList.FindIndex(str => str.StartsWith(label));
+    if (index == -1)
+      lenseList.Add(set);
+    else
+      lenseList[index] = set;
+  }
+  else
+  {
+    int index = lenseList.FindIndex(str => str.StartsWith(label));
+    if (index != -1) lenseList.RemoveAt(index);
+  }
+
+  lenses[lenseSum] = lenseList;
 }
+
+Console.WriteLine($"Part One: {overAllSum}");
+
+overAllSum = 0;
+foreach(var lense in lenses)
+{
+  long focalLength = lense.Key + 1;
+
+  for (int i = 0; i < lense.Value.Count; i++)
+  {
+    string set = lense.Value[i];
+    int value = int.Parse(set.Substring(set.IndexOf('=') + 1));
+    overAllSum += value * (i + 1) * focalLength;
+  }
+}
+
+Console.WriteLine($"Part Two: {overAllSum}");
